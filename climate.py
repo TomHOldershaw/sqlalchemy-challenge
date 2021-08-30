9921befbc6e50d8bb2d2a6fc6295b950e108d1df
TomHOldershaw <84713490+TomHOldershaw@users.noreply.github.com>
2021-08-30 08:33:48 +0100

    First fully working version of app

--- climate.py
+++ climate.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, func
 from sqlalchemy.orm import Session
 from sqlalchemy.ext.automap import automap_base
 import os
+import numpy as np
+import datetime as dt
 
 #SQLAlchemy
 #Set Base
@@ -20,6 +22,9 @@ print(Base.classes.keys())
 #Reflect tables
 Measurement = Base.classes.measurement
 Station = Base.classes.station
+
+# Specify today's date
+today = dt.datetime.strptime(str(dt.date.today()), '%Y-%m-%d')
 
 # Flask
 # Set up app
@@ -80,12 +85,65 @@ def stations():
 
 @app.route("/api/v1.0/tobs")
 def tobs():
-    return("Temperature")
+    # Open session
+    session = Session(engine)
+
+    # Retrieve latest date and format
+    recdate = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    date = dt.datetime.strptime(recdate[0], '%Y-%m-%d')
+    recyear = date.year
+    recmonth = str(date.month).zfill(2)
+    recday = str(date.day).zfill(2)
+
+    # Calculate the date one year from the last date in data set.
+    lastyear = recyear -1
+    lastdate = f"{lastyear}-{recmonth}-{recday}"
+
+    # Get most active station
+    frequent = (session
+            .query(Measurement.station, func.count(Measurement.station))
+            .group_by(Measurement.station)
+            .order_by(func.count(Measurement.station).desc())
+            .all()
+           )
+    most_frequent = frequent[0][0]
+
+    # Perform a query to retrieve the data and tobs
+    results = (session
+        .query(Measurement.date, Measurement.tobs)
+        .filter(Measurement.date >= lastdate)
+        .filter(Measurement.station == most_frequent)
+        .order_by(Measurement.date)
+        .all()
+       )
+
+    # Close session
+    session.close()
+
+    # Return data as simple list of results
+    return(jsonify(list(np.ravel(results))))
+    
 
 @app.route("/api/v1.0/<start>")
 @app.route("/api/v1.0/<start>/<end>")
-def temp(start, end):
-    return("Start/end")
+def temp(start, end=today):
+    # Open session
+    session = Session(engine)
 
+    # Perform a query to retrieve the data
+    results = (session
+        .query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs))
+        .filter(Measurement.date <= end)
+        .filter(Measurement.date >= start)
+        .order_by(Measurement.date)
+        .all()
+       )
+
+    session.close()
+
+    # Return data
+        temp_dict = {"Minimum" : results[0][0], "Average": results[0][1], "Maximum" : results[0][2]}
+        return(jsonify(temp_dict))
+    
 if __name__ == '__main__':
     app.run(debug=True)
